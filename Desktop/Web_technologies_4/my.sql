af03bc0c2a887a7e7c45ceaeae7bd27716a685f3
CristinaZarinsh <113066188+CristinaZarinsh@users.noreply.github.com>
2022-12-19 20:06:59 +0300

    Add files via upload

--- Desktop/Web_technologies_4/my.sql
+++ Desktop/Web_technologies_4/my.sql
@@ -3,21 +3,21 @@
 CREATE TABLE CLASSMATES (
   empId INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  age INTEGER NOT NULL
+  age INTEGER NOT NULL,
   address TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO CLASSMATES VALUES (0001, 'Екатерина', '17', 'Москва');
-INSERT INTO CLASSMATES VALUES (0002, 'Олег', '35', 'Воронеж');
-INSERT INTO CLASSMATES VALUES (0003, 'Анна', '28', 'Москва');
-INSERT INTO CLASSMATES VALUES (0004, 'Василий', '30', 'Липецк');
-INSERT INTO CLASSMATES VALUES (0005, 'Дарья', '20', 'Тверь');
-INSERT INTO CLASSMATES VALUES (0006, 'Ева', '16', 'Воронеж');
-INSERT INTO CLASSMATES VALUES (0007, 'Максим', '25', 'Москва');
-INSERT INTO CLASSMATES VALUES (0008, 'Ольга', '31', 'Москва');
-INSERT INTO CLASSMATES VALUES (0009, 'Дмитрий', '23', 'Омск');
-INSERT INTO CLASSMATES VALUES (0010, 'Ирина', '27', 'Москва');
+INSERT INTO CLASSMATES VALUES (1, 'Екатерина', '17', 'Москва');
+INSERT INTO CLASSMATES VALUES (2, 'Олег', '28', 'Воронеж');
+INSERT INTO CLASSMATES VALUES (3, 'Анна', '18', 'Москва');
+INSERT INTO CLASSMATES VALUES (4, 'Василий', '30', 'Липецк');
+INSERT INTO CLASSMATES VALUES (5, 'Дарья', '20', 'Тверь');
+INSERT INTO CLASSMATES VALUES (6, 'Ева', '16', 'Воронеж');
+INSERT INTO CLASSMATES VALUES (7, 'Максим', '25', 'Москва');
+INSERT INTO CLASSMATES VALUES (8, 'Ольга', '30', 'Москва');
+INSERT INTO CLASSMATES VALUES (9, 'Дмитрий', '23', 'Омск');
+INSERT INTO CLASSMATES VALUES (10, 'Ирина', '27', 'Москва');
 
 -- fetch 
-SELECT * FROM CLASSMATES WHERE dept = 'Москва';
+SELECT name FROM CLASSMATES WHERE address = 'Москва' AND age >= 18 AND age < 30;
